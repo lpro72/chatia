85b99c3305273a047ea27e9300d0e28335371780
Luc Provencher <lucprovencher72@gmail.com>
2025-09-03 21:41:48 -0400

    Correction d'erreur pour les Échec

--- chatia/chatia.go
+++ chatia/chatia.go
@@ -14,17 +14,25 @@ import (
 * main
 *******************/
 func main() {
-    brain.DumpMemory("Text")
-    brain.LearnFromString("This is a test", "Text")
-    brain.LearnFromString("Ceci est un test", "Text")
-    brain.LearnFromString("The brain work", "Text")
-    brain.LearnFromString("Test with the word these", "Text")
-    brain.DumpMemory("Text")
+    fmt.Println("***** Enter main *****")
+//    brain.LearnFromString("This is a test", "Text")
+//    brain.LearnFromString("Ceci est un test", "Text")
+//    brain.LearnFromString("The brain work", "Text")
+//    brain.LearnFromString("Test with the word these", "Text")
+//    brain.DumpMemory("Text")
+
+    brain.LearnFromString("1.e4 e5 2.Nf3 Nc6", "Chess")
+    brain.DumpMemory("Chess")
     for i := 0; i < 10; i++ {
         fmt.Printf("Test %d\n-------\n", i)
-        fmt.Println(brain.Exec("GetRandomWordFromLetterCell", "Text"))
-        fmt.Println(brain.Exec("GetRandomWordFromWordCell", "Text"))
+//        fmt.Println(brain.Exec("GetRandomWordFromLetterCell", "Text"))
+//        fmt.Println(brain.Exec("GetRandomWordFromWordCell", "Text"))
+
+        brain.Exec("test", "Chess")
     }
+
+    fmt.Println("***** Exit main *****")
+
     os.Exit(error.SUCCESS)
 }
 
--- modules/brain/chess.go
+++ modules/brain/chess.go
@@ -6,7 +6,8 @@ package brain
 * Import
 *******************/
 import (
-    "chatia/modules/brain/cell"
+    "fmt"
+//    "chatia/modules/brain/cell"
 )
 
 /*******************
@@ -20,25 +21,35 @@ type ChessStruct struct {
 * LearnChess
 *******************/
 func LearnChess(data []byte, firstCell any) {
-    LearnText(string(data), firstCell.(*TextStruct))
+    fmt.Println("***** Enter LearnChess *****")
+
+    fmt.Println("***** Exit LearnChess *****")
 }
 
 /*******************
 * ChessFactory
 *******************/
 func ChessFactory(brain *BrainStruct)  {
+    fmt.Println("***** Enter ChessFactory *****")
+
     brain.FirstCell = new(ChessStruct)
     brain.Learn = LearnChess
     brain.DumpMemory = DumpMemoryChess
     brain.Exec = ExecChess
+
+    fmt.Println("***** Exit ChessFactory *****")
 }
 
 /*******************
 * init
 *******************/
 func init() {
-    AddBrainFactory("Chess", TextFactory)
-    CreateBrainContext("Text")
+    fmt.Println("***** Enter init *****")
+
+    AddBrainFactory("Chess", ChessFactory)
+    CreateBrainContext("Chess")
+
+    fmt.Println("***** Exit init *****")
 }
 
 /*******************
@@ -46,6 +57,10 @@ func init() {
 *******************/
 // Learn some texte
 func ExecChess(command string, extraVar ...any) string {
+    fmt.Println("***** Enter ExecChess *****")
+
+    fmt.Println("***** Exit ExecChess *****")
+
     return ""
 }
 
